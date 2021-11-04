8338710e3117eac9266f726a4ac07d28092c523f
rinalditomas <rinalditomas@gmail.com>
2021-11-04 09:09:42 +0000

    navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import './Navbar.css'
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-    // const [showBar, setShowBar] = useState(false)
+  const [navbarMobile, setNavbarMobile] = useState(false);
+  const [showBar, setShowBar] = useState(false);
 
-    // useEffect(() => {
-    //    window.addEventListener('scroll', ()=>{
-    //     if (window.scrollY > 100) setShowBar(true);
-    //     else setShowBar(false);
-    //   });
-    //   return () => {
-    //     window.removeEventListener("scroll");
-    //   };
-    // }, [])
+  const handleClick = () => setNavbarMobile(!navbarMobile);
+  const closeMobileMenu = () => setNavbarMobile(false);
 
-    return (
-        <div className= 'navbar'>
-            <div className="logo">
-                <img src="https://pngimage.net/wp-content/uploads/2018/05/example-logo-png-1.png" alt="" />
-            </div>
-            <ul className='desktop'>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>The Team</li>
-                <li>Services</li>
-            </ul>
-        </div>
-    )
-}
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) setShowBar(true);
+      else setShowBar(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
 
-export default Navbar
+  return (
+    <div
+      className={`navbar ${
+        navbarMobile
+          ? (showBar && "blue-color") || "blue-color"
+          : showBar && "navbar-white"
+      }`}
+    >
+      <div className="navbar-logo">
+        <img
+          src="https://pngimage.net/wp-content/uploads/2018/05/example-logo-png-1.png"
+          alt=""
+        />
+      </div>
+      <div className="menu-icon" onClick={handleClick}>
+        {navbarMobile ? (
+          <ion-icon id="close" name="close-outline"></ion-icon>
+        ) : (
+          <ion-icon id="open" name="menu-outline"></ion-icon>
+        )}
+      </div>
+      <ul className={navbarMobile ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">Home</li>
+        <li className="nav-item">About Us</li>
+        <li className="nav-item">The Team</li>
+        <li className="nav-item">Services</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
